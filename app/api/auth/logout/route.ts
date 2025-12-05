46701f1bc82f99d36e5aedd22285210b222fc9ba
KyawMinnThant <kyawminnthant12345@gmail.com>
2025-12-05 13:53:58 +0630

    commit end

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -4,12 +4,7 @@ export async function GET() {
   const cookieStore = await cookies();
 
   // To delete a cookie, you set it again with maxAge 0 or expires in the past
-  cookieStore.set({
-    name: "uid",
-    value: "",
-    path: "/",
-    maxAge: 0,
-  });
+  cookieStore.delete("uid");
 
   return new Response(JSON.stringify({ message: "UID cookie deleted" }), {
     status: 200,
